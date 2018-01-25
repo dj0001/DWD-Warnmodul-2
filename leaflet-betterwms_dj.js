9267438d87830310ae3915a6835a55b030395272
dj <dj0@gmx.de>
2018-01-25 18:50:24 +0100

    autoupdate marker

--- leaflet-betterwms_dj.js
+++ leaflet-betterwms_dj.js
@@ -74,6 +74,7 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
 
   // Warnmodul2: angepasste Funktion zum Aufbereiten der Geoserver-Antwort (Auswahl von bestimmten properties) und zur Anzeige als Popup
   showGetFeatureInfoJson: function (latlng, data) {
+    marker.unbindPopup(); marker.setLatLng(latlng)  //feedback
     if ( data.features[0] == null ) { return 0 };
     var content="<h2>Amtliche Warnung</h2>";
     var color={Minor:"yellow",Moderate:"orange",Severe:"red",Extreme:"DarkRed"}  //
