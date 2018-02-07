80ff9e7c697f21889bd5a0a0ff17fa55a9dc520e
dj <dj0@gmx.de>
2018-02-07 13:23:07 +0100

    notification

---
+++ src/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('../index.html')
+  );
+});
