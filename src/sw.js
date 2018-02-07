83d8c828f545eb8ee1ad509896ed4771f32887f7
dj <dj0@gmx.de>
2018-02-07 14:00:22 +0100

    notification
    bugfix

--- src/sw.js
+++ src/sw.js
@@ -4,6 +4,6 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.openWindow('../index.html')
+    clients.openWindow('./index.html')
   );
 });
