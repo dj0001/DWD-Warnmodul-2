4fb0e3aa1583e59ce2d3bda53242f19063f28493
dj <dj0@gmx.de>
2018-02-07 13:32:08 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('notificationclick', function(event) {
-  console.log('[Service Worker] Notification click Received.');
-
-  event.notification.close();
-
-  event.waitUntil(
-    clients.openWindow('./index.html')
-  );
-});
