0460e54847efaa1bb57dd693ab15d57965e634c9
dj <dj0@gmx.de>
2018-02-08 07:29:46 +0100

    focus

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
 self.addEventListener('notificationclick', function(event) {
   console.log('[Service Worker] Notification click Received.');
-
   event.notification.close();
 
-  event.waitUntil(
-    clients.openWindow('./index.html')
-  );
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
 });
