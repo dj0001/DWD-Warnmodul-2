09d4b99a2ef7d67be9781ae705536921541684a2
dj <dj0@gmx.de>
2018-02-08 07:48:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,13 +6,13 @@ self.addEventListener('notificationclick', function(event) {
   // focuses if it is
   event.waitUntil(clients.matchAll({
     type: "window"
-  }).then(function(clientList) {
+  }).then(function(clientList) {console.log(clientList)
     for (var i = 0; i < clientList.length; i++) {
       var client = clientList[i];
       if (client.url == './' && 'focus' in client)
         return client.focus();
     }
     if (clients.openWindow)
-      return clients.openWindow('./');
+      return clients.openWindow('./index.html');
   }));
 });
