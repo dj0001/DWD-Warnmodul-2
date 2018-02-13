41cba13242ef47fbcc8983a1ef6fe74f9d99385f
dj <dj0@gmx.de>
2018-02-13 14:10:41 +0100

    reduce traffic on mobile

--- src/addons_dj.js
+++ src/addons_dj.js
@@ -7,7 +7,7 @@ var auto=300  //update-interval in s (alle 5 min; 2e6 no-updates)  //edit here
   auto=Math.max(300,auto); auto*=1000; if((navigator.connection||{}).type=="cellular") auto*=2  //reduce traffic
   var tID,t1; warnlayer._marker.on('move', function(e){clearTimeout(tID); t1=Date.now(); tID=setTimeout(function(){updatem(e)}, auto)});  //?1 watch location
   function updatem(e) {qs==1?karte.locate({setView:true, maxZoom:8}):warnlayer.getFeatureInfoJsonp({latlng:e.latlng})}
-  var t0=Date.now(); setInterval(function(){update()}, 3*auto)  //add update
+  var t0=Date.now(); setInterval(function(){update()}, 2*auto)  //add update
   document.addEventListener("visibilitychange", function(){if(!document.hidden) {update();if(Date.now()-t1>=auto) updatem({latlng:warnlayer._marker.getLatLng()})}}, false);
   
   function update() {if(Date.now()-t0>=3*auto && !document.hidden) {t0=Date.now(); warnlayer.setUrl("https://maps.dwd.de/geoproxy_warnungen/service/?" + Math.random());
