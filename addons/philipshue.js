5f80046434eb62ed8182d2bae461b289d4ed1e17
dj <dj0@gmx.de>
2018-05-09 06:48:41 +0200

    add user

--- addons/philipshue.js
+++ addons/philipshue.js
@@ -42,12 +42,13 @@ if ((localStorage||{}).bridge) bridge=localStorage.bridge;
 //else if(self.fetch) { fetch("https://www.meethue.com/api/nupnp").then(function(response)  //discover bridge
 // {response.json().then(function(data){if(data[0]) bridge=data[0].internalipaddress;changebri((data[0]?'':"not ")+"found\n");showLights({on:true})})} )} 
 else {changebri("addon successfully installed\n");   // ?4 disable
-// if(bridge.match("/api/")) showLights({on:true}); else
-// fetch(bridge+"/api",{method:"POST",body:'{"devicetype":""}',headers:{'Content-Type':'application/json'}}).then(function(response)  //press link button
-//  {response.json().then(function(data){if(data[0].success) bridge+="/api/"+data[0].success.username; showLights({on:true})})} )
  if(!bridge.match("/api")) bridge+="/api"
  if(self.fetch) fetch(bridge,{method:"GET"}).then(function(response)  //"http://localhost:8000/api//config"
-  {response.json().then(function(data){if(!data[0]) localStorage.bridge=bridge; else changebri("unauthorised user\n")})} )
+  {response.json().then(function(data){if(!data[0]) localStorage.bridge=bridge; else {
+   alert("unauthorised user\npress link button"); bridge=bridge.replace(/\/api.*/,'/api')
+   fetch(bridge,{method:"POST",body:'{"devicetype":""}',headers:{'Content-Type':'application/json'}}).then(function(response)
+   {response.json().then(function(data){if(data[0].success) localStorage.bridge=bridge+="/"+data[0].success.username})} ) }
+  })} )
   .catch(function(err){changebri("bridge not found\n")})
 }
 console.log(bridge)
